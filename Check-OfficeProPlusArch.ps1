b7b5d7071f06fcd8dec3f500f160338b92770d05
diablolot53 <diablolot53@gmail.com>
2019-10-29 10:54:10 -0400

    Added Check-OfficeProPlusArch.ps1
    -Script can be deployed with Intune to check the architecture of Office ProPlus installed on a workstation

---
+++ Check-OfficeProPlusArch.ps1
@@ -0,0 +1,32 @@
+﻿<#
+Office ProPlus Architecture Detection Script for Intune 
+Version 1.0
+Last Update - 10/29/2019
+
+PURPOSE
+    This script is used to detect the architecture of Office ProPlus
+    that is installed on a computer. It will report back to Intune 
+    success/failure depending on the architecture searched for.
+
+USE
+    Set the desired architecture to search for using the $DesiredArch variable
+    and then deploy the script using Intune. Any device with the desired architecture
+    will have a Success status. Any other architecture will return a Failed status.
+
+VARIABLES
+    $DesiredArch - Sets the Office architecture to search for.
+        x86 - 32-bit
+        x64 - 64-bit 
+#>
+
+#Variables
+$DesiredArch = "x86"
+
+If ((Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration).Platform -eq $DesiredArch){
+    Write-Host "Desired Office ProPlus architecture found - $DesiredArch"
+    Exit 0
+}
+Else{
+    Write-Error -Message "Could not find desired architecture - $DesiredArch"
+    Exit 1
+}
